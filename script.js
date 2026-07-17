a1a5a4e1e71ea090952d61dd257696983bbb043c
milana-nayak <nayakmilana@gmail.com>
2026-07-17 11:43:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ document.getElementById("scrollBtn").addEventListener("click", function () {
     behavior: "smooth"
   });
 });
+const contactForm = document.querySelector(".contact-form");
+
+contactForm.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    alert("Message sent successfully!");
+
+    contactForm.reset();
+
+});
